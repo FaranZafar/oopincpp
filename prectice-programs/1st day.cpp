63b81f33dcae88da9e2337f25bc630c039a5f78d
Faran <faranpannu02@gmail.com>
2026-03-03 08:20:58 +0500

    oop-prectice code

---
+++ prectice-programs/1st day.cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int num1,num2;
+//	cout<<"Enter your first Number:";
+//	cin>>num1;
+//	cout<<"Enter your second number:";
+//	cin>>num2;
+//	int c=num1*num2;
+//	cout<<"the answer is:"<<c<<endl;
+//}
---
+++ prectice-programs/array+function.cpp
@@ -0,0 +1,125 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int size;
+//	cout<<"Enter Size of array:";
+//	cin>>size;
+//	
+//     int arr[size];
+//     int arr1[size];
+//    
+//    for(int i=1;i<=size;i++){
+//    	cout<<"Enter number:";
+//    	cin>>arr[i];
+//	}
+//	cout<<"------------------------------"<<endl;;
+//	for(int i=1;i<=size;i++){
+//        cout<<"number is:"<<arr[i]<<endl;
+//	}
+//	cout<<"------------------------------"<<endl;;
+//	for(int i=1;i<=size;i++){
+//      arr1[i]=arr[i];
+//	}
+//	cout<<"-----------arr1----------------- is:"<<endl;
+//	for(int i=1;i<=size;i++){
+//        cout<<"number is:"<<arr1[i]<<endl;
+//	}
+//	
+//	
+//	
+//}
+//============================sequre of 10 number ==================
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int size;
+//	cout<<"Enter size of array:";
+//	cin>>size;
+//	int arr1[size];
+//	int arr2[size];
+//	int sum;
+//
+//	for(int i=0;i<size;i++){
+//		cout<<"Enter number:";
+//		cin>>arr1[i];
+//	}
+//	cout<<"----------------------------------"<<endl;
+//	for(int i=0;i<size;i++){
+//		arr2[i]= arr1[i] * arr1[i];
+//	}
+//	for(int i=0;i<size;i++){
+//		cout<<"Square of number is:"<<arr2[i]<<endl;
+//	}
+//	for(int i=0;i<size;i++){
+//	  sum=sum+arr2[i];
+//	
+//		
+//	}
+//	cout<<"----------------------------------------"<<endl;
+//	cout<<"the sum is:"<<sum;
+//}
+////========================= array+function===========================================
+//#include<iostream>
+//using namespace std;
+//void storedata(int arr[],int n){
+//	for(int i=0;i<n;i++){
+//		cout<<"enter number:";
+//		cin>>arr[i];
+//	}
+//}
+//void square(int arr[],int n){
+//	for(int i=0;i<n;i++){
+//	   arr[i]=arr[i]*arr[i];
+//	}
+//}
+//void sum(int arr[],int n){
+//	int sum=0;
+//	for(int i=0;i<n;i++){
+//	  sum=sum+arr[i];
+//
+//	}
+//		cout<<"sum:"<<sum;
+//}
+//void display(int arr[],int n){
+//	for(int i=0;i<n;i++){
+//	   cout<<"the square numbers:"<<arr[i]<<endl;
+//	  
+//}
+//}
+//
+//int main()
+//{
+//	int size;
+//    cout<<"enter size of array:";
+//	cin>>size;
+//	int arr1[size];
+//	//function Calling
+//	storedata(arr1,size);
+//	cout<<"---------------------------------"<<endl;
+//
+//	square(arr1,size);
+//	display(arr1,size);	
+//	cout<<"---------------------------------"<<endl;
+//    sum(arr1,size);
+//    cout<<endl;
+//    cout<<"---------------------------------"<<endl;
+//
+//	
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
