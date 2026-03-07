279219d8d72cfa71dde01e68220d285557f202c0
Faran <faranpannu02@gmail.com>
2026-03-07 17:50:21 +0500

    oop prectice code

---
+++ prectice-programs/calcluter(switch).cpp
@@ -0,0 +1,67 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a,b;
+//	char choice;
+//	cout<<"Enter first number=";
+//	cin>>a;
+//	cout<<"Enter second number=";
+//	cin>>b;
+//	cout<<"Enter operators (+,-,*,/)=";
+//	cin>>choice;
+//    switch(choice)
+//    {
+//    	case'+':
+//       cout<<"Result:"<<a+b<<endl;
+//       break;
+//       case'-':
+//       	cout<<"Result:"<<a-b<<endl;
+//       	break;
+//       	 case'*':
+//       	cout<<"Result:"<<a*b<<endl;
+//       	break; case'/':
+//       	cout<<"Result:"<<a/b<<endl;
+//       	break;
+//       	  	default:
+//       	  		cout<<"Error";
+//	}
+//	  	
+//   }
+#include<iostream>
+using namespace std;
+int main()
+{
+	int hours,a,b;
+	char user,salary,tax;
+	cout<<"Enter working hours=";
+	cin>>hours;
+	cout<<"Enter your designation=";
+	cin>>user;
+	if(user=='manager');
+	{
+		cout<<"Your salary is ="<<hours*5000<<endl;
+//		
+		cout<<"Your tax is =25%"<<endl;
+		cout<<"Your salary after tax="<<hour*5000*25%/100;
+//		cout<<"Your salary after tax="<<salary*/100;
+//	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
