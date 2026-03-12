17c0a9b278db3e452f465b70e1d02bc2a1edc28e
Faran <faranpannu02@gmail.com>
2026-03-12 06:46:19 +0500

    oop prectice code

---
+++ prectice-programs/Mid-revision.cpp
@@ -0,0 +1,156 @@
+//#include<iostream>
+//using namespace std;
+//void name(){
+//	cout<<"Welcome faran pannu";
+//}
+//int main(){
+//	name();
+//}
+//================================================
+//#include<iostream>
+//using namespace std;
+//void display(){
+//	for(int i=1;i<=10;i++)
+//	{
+//		cout<<"number:"<<i<<endl;
+//	}
+//	
+//}
+//int main(){
+//	display();
+//}
+//================================================
+//#include<iostream>
+//using namespace std;
+//void display(){
+//	for(int i=1;i<=10;i++){
+//		cout<<"hallo world "<<i<<endl;
+//	}
+//}
+//int main(){
+//	display();
+//}
+//=================================================
+//#include<iostream>
+//using namespace std;
+//void reverse(int *a,int *b){
+//	int temp=*a;
+//	*a=*b;
+//	*b=temp;
+//}
+//int main(){
+//	int num1,num2;
+//	cout<<"enter number1:";
+//	cin>>num1;
+//	cout<<"enter number2:";
+//	cin>>num2;
+//	reverse(&num1,&num2);
+//	cout<<"number1:"<<num1<<endl<<"number2:"<<num2;
+//	
+//}
+//======================--1--================================
+
+//#include<iostream>
+//using namespace std;
+//void display(){
+//	cout<<"welcome faran pannu";
+//}
+//int main(){
+//	display();
+//}
+
+//======================--2--================================
+//#include<iostream>
+//using namespace std;
+//void display(){
+//	for(int i=1;i<=10;i++){
+//		cout<<"number:"<<i<<endl;
+//	}
+//}
+//int main(){
+//	display();
+//}
+
+//=======================--3--===============================
+
+//#include<iostream>
+//using namespace std;
+//void display(){
+//	for(int i=1;i<=10;i++){
+//		cout<<"hallo world"<<endl;
+//	}
+//}
+//int main(){
+//	display();
+//}
+
+//=======================--4--===============================
+
+//#include<iostream>
+//using namespace std;
+//void circleRadius(int r){
+//	cout<<"the radius of circle is:"<<3.14*r*r;
+//}
+//int main(){
+//	int rad;
+//	cout<<"enter radius of circle:";
+//	cin>>rad;
+//	circleRadius(rad);
+//}
+
+//=======================--5--===============================
+
+//#include<iostream>
+//using namespace std;
+//int add(int a,int b,int c=0,int d=0,int e=0){
+//	return a+b+c+d+e;
+//}
+//int main(){
+//	cout<<"call with two arguments:"<<add(4,5)<<endl;
+//	cout<<"call with three arguments:"<<add(4,5,5)<<endl;
+//	cout<<"call with three arguments:"<<add(4,5,6)<<endl;
+//	cout<<"call with four arguments:"<<add(4,5,7,5)<<endl;
+//	cout<<"call with five arguments:"<<add(4,5,8,3,5)<<endl;
+//}
+
+//=======================--6--===============================
+
+//#include<iostream>
+//using namespace std;
+//int square(int a){
+//	return a*a;
+//}
+//double square(double a){
+//	return a*a;
+//}
+//int main(){
+//	cout<<"for int:"<<square(5)<<endl;
+//	cout<<"for double:"<<square(5.5);
+//}
+
+//=======================--6--===============================
+
+//#include<iostream>
+//using namespace std;
+//int swap(int *a,int *b){
+//	int temp=*a;
+//	*a=*b;
+//	*b=temp;
+//}
+//int main(){
+//	int num1,num2;
+//	cout<<"enter number 1:";
+//	cin>>num1;
+//	cout<<"enter number 2:";
+//	cin>>num2;
+//	swap(&num1,&num2);
+//	cout<<"number 1:"<<num1<<endl<<"number 2:"<<num2;
+//}
+
+//=======================--7--===============================
+
+
+
+
+
+
---
+++ prectice-programs/overloading function.cpp
@@ -0,0 +1,156 @@
+//#include<iostream>
+//using namespace std;
+//void max(int a,int b){
+//	if(a>b){
+//		cout<<"geater value is:"<<a;
+//	}else{
+//		cout<<"greater value is:"<<b;
+//	}
+//}
+//void max(double a,double b){
+//	if(a>b){
+//		cout<<"geater value is:"<<a;
+//	}else{
+//		cout<<"greater value is:"<<b;
+//	}
+//}
+//void max(int a,int b,int c){
+//	if(a>b && a>c){
+//		cout<<"geater value is:"<<a;
+//	}
+//	else if(b>c && b>a){
+//		cout<<"greater value is:"<<b;
+//	}else if(c>a && c>b){
+//		cout<<"greater value is: "<<c;
+//	}else{
+//		cout<<"invalid information";
+//	}
+//}
+//void max(double a,double b,double c){
+//	if(a>b && a>c){
+//		cout<<"geater value is:"<<a;
+//	}else if(b>c && b>a){
+//		cout<<"greater value is:"<<b;
+//	}else if(c>a && c>b){
+//		cout<<"greater value is:"<<c;
+//	}else{
+//		cout<<"invalid information";
+//	}
+//}
+//void max(int a,int b,int c,int d){
+//	if(a>b && a>c && a>d){
+//		cout<<"geater value is:"<<a;
+//	}
+//	else if(b>a && b>c && b>d){
+//		cout<<"greater value is:"<<b;
+//	}else if(c>a && c>b && c>d){
+//		cout<<"greater value is: "<<c;
+//	}else if(d>a && d>b && d>c){
+//		cout<<"greater value is:"<<d;
+//	}else{
+//		cout<<"invalid information";
+//	}
+//}
+//void max(double a,double b,double c,double d){
+//	if(a>b && a>c && a>d){
+//		cout<<"geater value is:"<<a;
+//	}else if(b>a && b>c && b>d){
+//		cout<<"greater value is:"<<b;
+//	}else if(c>a && c>b && c>d){
+//		cout<<"greater value is:"<<c;
+//	}else if(d>a && d>b && d>c){
+//		cout<<"greater value is:"<<d;
+//	}else{
+//		cout<<"invalid information";
+//	}
+//}
+//int main(){
+//     double num1,num2;
+//     double num3=0;
+//     double num4=0;
+//     cout<<"Enter value1:";
+//     cin>>num1;
+//     cout<<"Enter value2:";
+//     cin>>num2;
+//     cout<<"Enter value3:";
+//     cin>>num3;
+//     cout<<"Enter value4:";
+//     cin>>num4;
+//     max(num1,num2,num3,num4);
+//}
+
+//======================================================
+//#include<iostream>
+//using namespace std;
+//void area(int r){
+//	cout<<"area of circle:"<< 3.14 * r * r<<endl;
+//}
+//void area(int lenght, int width, int hight){
+//	cout<<"area of rectangular:"<< lenght * width * hight;
+//}
+//
+//int main(){
+//	int radius,l=0,w=0,h=0;
+//	cout<<"enter radius to find area of circle:";
+//	cin>>radius;
+//	cout<<"enter lenght to find area of rectangular:";
+//	cin>>l;
+//	cout<<"enter width to find area of rectangular:";
+//	cin>>w;
+//	cout<<"enter hight to find area of rectangular:";
+//	cin>>h;
+//	area(radius);
+//	area(l,w,h);
+//}
+
+//===========================array=======================
+//array is a data structure.
+//same datatype
+//logically related
+//adjacent memoryspace
+//random access
+
+//int size=10;
+//here 10 is equal to size of array;
+//int arr[size];
+//for loop is used data adding and printing
+//for(int i=0;i<size;i++){
+//	cin>>arr[i];
+//}
+//for(int i=0;i<size;i++){
+//	cout<<arr[i]<<endl;
+//}
+
+//for(int i=0;i<size;i++){
+//	arr[i]=arr2[i]
+//}
+//==============================================j
+//take array from user and calclute a square of that array and paste another array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
