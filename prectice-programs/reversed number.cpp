7daa1a1b5c4e899f795b0e5f562a3394d088c7b7
Faran <faranpannu02@gmail.com>
2026-03-15 15:15:47 +0500

    oop prectice code

---
+++ prectice-programs/reversed number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,reversed=0;
+	cout<<"enter number:";
+	cin>>num;
+	while(num>0){
+		int digit=num % 10;
+		reversed=reversed*10+digit;
+		num=num/10;
+	}
+	cout<<"reversed number:"<<reversed;
+}
---
+++ prectice-programs/structure.cpp
@@ -0,0 +1,49 @@
+//#include<iostream>
+//using namespace std;
+//struct Student
+//{
+//	string name;
+//	int rollnumber,marks;
+//	float avg;
+//	char grade;
+//	
+//};
+//int main()
+//{
+//	Student obj1;
+//	cout<<"Enter your name:";
+//	cin>>obj1.name;
+//	cout<<"Enter your Rollnumber:";
+//	cin>>obj1.rollnumber;
+//	cout<<"Enter your marks:";
+//	cin>>obj1.marks;
+//	cout<<"Enter your percentage:";
+//	cin>>obj1.avg;
+//	cout<<"Enter your grade:";
+//	cin>>obj1.grade;
+//	cout<<"you enter the following detailes.";
+//	cout<<"name:"<<obj1.name<<endl;
+//	cout<<"Rolnumber:"<<obj1.rollnumber<<endl;
+//	cout<<"marks:"<<obj1.marks<<endl;
+//	cout<<"percentage:"<<obj1.avg<<endl;
+//	cout<<"grade:"<<obj1.grade;
+//	
+//	
+//}
+//============================using array in structure=============
+#include<iostream>
+using namespace std;
+int main(){
+	
+}
+
+
+
+
+
+
+
+
+
+
+
