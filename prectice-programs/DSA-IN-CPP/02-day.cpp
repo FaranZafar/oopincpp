6e274ff3e42ad50bee97dc9e424b570209dcd92c
Faran <faranpannu02@gmail.com>
2026-03-16 09:00:13 +0500

    DSA prectice code

---
+++ prectice-programs/DSA-IN-CPP/02-day.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main()
+{    
+    // int age=25;  //4 bytes space
+    // char grade='a'; // 1 byte space 
+    // float PI=3.14f; // 4 bytes space
+    // bool isSafe= false; // 1 byte space
+    //  // true=1, false=0
+    //  double price=100.99;// 8 bytes space
+   
+    //   cout<<isSafe<<endl;
+
+
+    //type casting concept
+      //1.conversion
+    // char grade= 'a';//97
+    // int value =grade;
+    // cout<<value<<endl;
+      //2.casting
+    //  double price=100.99;
+    //  int newPrice=price;
+    //  cout<<newPrice<<endl; 
+  
+    //input in cpp
+  //   int age;
+  //   cout<<"enter age:";
+  //   cin>>age;
+  //   cout<<"age:"<<age;
+  // return 0;
+  // double a=10,b=3;
+  // cout<<"division = "<<(a/b)<<endl;
+  //==========================practice question=============
+  int a,b,sum;
+  cout<<"enter number 1:";
+  cin>>a;
+  cout<<"enter number 2:";
+  cin>>b;
+  sum=a+b;
+  cout<<"sum = "<<sum;
+}
+
+// boilerplate code --comment 
