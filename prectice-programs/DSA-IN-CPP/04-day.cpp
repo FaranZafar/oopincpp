75bd84aaf2fdaaf616ab954a8f1a3fddc0e9f781
Faran <faranpannu02@gmail.com>
2026-03-18 12:08:59 +0500

    DSA prectice code

---
+++ prectice-programs/DSA-IN-CPP/04-day.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"hallo world!";
+    //==============================
+    // int num1,num2,sum;
+    // cout<<"Enter number 1:";
+    // cin>>num1;
+    // cout<<"Enter number 2:";
+    // cin>>num2;
+    // cout<<"sum :"<<num1+num2;
+    //==============================
+    // string name;
+    // int age;
+    // cout<<"Enter your name :";
+    // cin>>name;
+    // cout<<"enter your age:";
+    // cin>>age;
+    // cout<<"Your name is "<<name<<". your are "<<age<<" old.";
+    //==============================
+    // int num1,num2,swap;
+    // cout<<" enter number 1:";
+    // cin>>num1;
+    // cout<<"enter number 2:";
+    // cin>>num2;
+    // swap=num1;
+    // num1=num2;
+    // num2=swap;
+    // cout<<"number 1:"<<num1<<endl;
+    // cout<<"number 2:"<<num2<<endl;
+    //==============================
+    // int num1;
+    // cout<<"Enter number to check even or odd:";
+    // cin>>num1;
+    // if(num1%2==0){
+    //     cout<<"number is even";
+
+    // }else{
+    //     cout<<"number is odd";
+    // }
+    //==============================
+    // int num1;
+    // cout<<"enter number to check positive/negtive/zero:";
+    // cin>>num1;
+    // if(num1>=1 & num1!=0){
+    //     cout<<"number is positive";
+    // }else if(num1<1 & num1!=0){
+    //     cout<<"number is negtive";
+    // }else if(num1==0){
+    //     cout<<"number is zero";
+    // }
+    int num1,num2;
+    cout<<"enter two numbers:";
+    cin>>num1,num2;
+    if(num1>num2){
+        cout<<"number 1 is greater";
+
+    }else{
+        
+            cout<<"number 2 is greater";
+        
+    }
+    return 0;
+
+}
